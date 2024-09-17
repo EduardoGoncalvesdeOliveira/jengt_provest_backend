ff80fd145db78e2d58c0902c65d7f896dc3981b8
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-09-17 11:48:30 -0300

    refactor: edição dos detalhes

--- model/DAO/professor.js
+++ model/DAO/professor.js
@@ -17,12 +17,11 @@ const insertProfessor = async(dadosProf) => {
     try {
         let sql
     
-        sql = `insert into tbl_professores (nome, email, telefone, senha, icone_perfil, status)values(
+        sql = `insert into tbl_professor (nome, email, senha, icone_id, status)values(
                 '${dadosProf.nome}',
                 '${dadosProf.email}',
-                '${dadosProf.telefone}',
                 md5('${dadosProf.senha}'),
-                '${dadosProf.icone_perfil}',
+                '${dadosProf.icone_id}',
                true
             )`
 
@@ -47,11 +46,10 @@ const updateProfessor = async(dadosProf, id) => {
     try {
         let sql 
 
-        sql = `update tbl_professores set 
+        sql = `update tbl_professor set 
                                             nome = "${dadosProf.nome}",
                                             email = "${dadosProf.email}",
-                                            telefone = "${dadosProf.telefone}",
-                                            foto_perfil = "${dadosProf.foto_perfil}",
+                                            icone_id = "${dadosProf.icone_id}",
                                             where id = ${id}`
 
         let result = await prisma.$executeRawUnsafe(sql)
@@ -72,7 +70,7 @@ const updateProfessor = async(dadosProf, id) => {
 // delete/put: método put apenas trocando o status, para "esconder" um prof filtrando pelo ID
 const updateDeleteProfessor = async(id) => {
     try {
-        let sql = `update tbl_professores set status = false where id = ${id}`
+        let sql = `update tbl_professor set status = false where id = ${id}`
 
         // executa o scriptSQL no BD e recebe o retorno dos dados na variável
         let rsProf = await prisma.$executeRawUnsafe(sql)
@@ -87,7 +85,7 @@ const updateDeleteProfessor = async(id) => {
 // put: método put apenas trocando o status, para aparecer um prof antes escondido
 const updateRecoverProfessor = async(id) => {
     try {
-        let sql = `update tbl_professores set status = true where id = ${id}`
+        let sql = `update tbl_professor set status = true where id = ${id}`
 
         // executa o scriptSQL no BD e recebe o retorno dos dados na variável
         let rsProf = await prisma.$executeRawUnsafe(sql)
@@ -103,7 +101,7 @@ const updateRecoverProfessor = async(id) => {
 const selectAllProfessores = async () => {
 
     try {
-        let sql = 'select nome, email, senha, telefone from tbl_professores where status = true order by nome asc;'
+        let sql = 'select nome, email, senha from tbl_professor where status=true order by nome asc'
     
         // $queryrawUnsafe(‘encaminha apenas a variavel’)
         // $queryRaw(‘codigo digitado aqui’)
@@ -124,7 +122,7 @@ const selectByIdProfessor = async (id) => {
     try {
 
         // realiza a busca do prof pelo id
-        let sql = `select * from tbl_professores where id=${id} and status=true`
+        let sql = `select * from tbl_professor where id=${id} and status=true`
 
         // executa no DBA o script SQL
         let rsProf = await prisma.$queryRawUnsafe(sql)
@@ -139,7 +137,7 @@ const selectByIdProfessor = async (id) => {
 const selectByNome = async (nome) => {
     
     try {
-        let sql = `select * from tbl_professor where nome like '%${nome}%' where status=true`
+        let sql = `select * from tbl_professor where nome like '%${nome}%' and status=true`
     
         // executa o scriptSQL no BD e recebe o retorno dos dados na variável rsAdmin
         let rsProf = await prisma.$queryRawUnsafe(sql)
@@ -155,7 +153,7 @@ const selectByNome = async (nome) => {
 const selectLastId = async () => {
     try {
 
-        let sql = 'select cast(last_insert_id() as DECIMAL) as id from tbl_professores limit 1' 
+        let sql = 'select cast(last_insert_id() as DECIMAL) as id from tbl_professor limit 1' 
 
         let rsProf = await prisma.$queryRawUnsafe(sql)
 
@@ -184,7 +182,7 @@ const updateProfSenha = async (dadosProf, idProf) => {
 const selectValidacaoProf = async (email, senha) => {
 
     try {
-        let sql = `select nome, email from tbl_usuarios where email = '${email}' and senha = md5('${senha}')`
+        let sql = `select nome, email from tbl_professor where email = '${email}' and senha = md5('${senha}')`
         let rsProf = await prisma.$queryRawUnsafe(sql)
         return rsProf        
     } catch (error) {
